0783906da011eceb00daa335aefcae5bfbae28ae
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2024-10-03 16:32:46 -0700

    bugfix

--- src-tauri/src/define/project.rs
+++ src-tauri/src/define/project.rs
@@ -346,7 +346,7 @@ impl Project {
                         control.insert(event);
                         
                         let lines = if (std::env::var("UD_WORKAROUND")).is_ok() { 
-                            make_fnis_lines(
+                            [make_fnis_lines(
                                 &position.event,
                                 "",
                                 stage.extra.fixed_len > 0.0,
@@ -356,7 +356,18 @@ impl Project {
                                     }
                                     acc
                                 }),
-                            )
+                            ),
+                            make_fnis_lines(
+                                &position.event,
+                                &self.prefix_hash,
+                                stage.extra.fixed_len > 0.0,
+                                &position.anim_obj.split(',').fold(vec![], |mut acc, x| {
+                                    if !x.is_empty() {
+                                        acc.push(x.to_string());
+                                    }
+                                    acc
+                                }),
+                            )].concat()
                         } else {
                             make_fnis_lines(
                                 &position.event,
